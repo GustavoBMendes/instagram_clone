b6cae218a1d00fccddbb5d6d66b2d452522ecc19
GustavoBMendes <mendes.gustavob@gmail.com>
2020-05-28 13:23:23 -0300

    Tab navigator criado

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+const Tab = createBottomTabNavigator();
+
+function Home() {
+	return (
+		<View>
+			<Text>Home</Text>
+		</View>
+	)
+}
+
+function Perfil() {
+	return (
+		<View>
+			<Text>Perfil</Text>
+		</View>
+	)
+}
 
 export default class Feed extends Component {
 
 	render() {
 		return (
-			<View>
-
-			</View>
+			<Tab.Navigator>
+				<Tab.Screen name='Home' component={() => <Home title='Home'/>}/>
+				<Tab.Screen name='Perfil' component={() => <Perfil title='Perfil'/>}/>
+			</Tab.Navigator>
 		);
 	}
 
