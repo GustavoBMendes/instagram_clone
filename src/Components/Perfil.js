bbc4d93a4733522b08adda5c031133cd6501a533
GustavoBMendes <mendes.gustavob@gmail.com>
2020-06-17 17:51:06 -0300

    Ajustes tela perfil

--- src/Components/Perfil.js
+++ src/Components/Perfil.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Button } from 'react-native';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 
 import { infoPerfilUser } from '../Actions/AppActions';
 import { SafeAreaView } from 'react-navigation';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 /*
 <View>
@@ -29,18 +30,43 @@ import { SafeAreaView } from 'react-navigation';
 function Informacoes({ item }) {
 	console.log("PERFIL", item);
 	return (
-		<View>
-			<View>
-				<Text>{item.posts}</Text>
-				<Text>{item.seguidores}</Text>
-				<Text>{item.seguindo}</Text>
+		<View style={{ flex: 1 }}>
+			<View style={{ flexDirection: 'row' }}>
+				<View>
+					<Text style={{ marginLeft: 35, marginTop: 25 }}>FOTO</Text>
+				</View>
+
+				<View style={{ marginLeft: 25, marginTop: 17 }}>
+					<Text style={{ textAlign: 'center', fontWeight: 'bold', }}>{item.posts}</Text>
+					<Text>Publicações</Text>
+				</View>
+
+				<View style={{ marginLeft: 15, marginTop: 17 }}>
+					<Text style={{ textAlign: 'center', fontWeight: 'bold', }}>{item.seguidores}</Text>
+					<Text>Seguidores</Text>
+				</View>
+
+				<View style={{ marginLeft: 15, marginTop: 17 }}>
+					<Text style={{ textAlign: 'center', fontWeight: 'bold', }}>{item.seguindo}</Text>
+					<Text>Seguindo</Text>
+				</View>
 			</View>
-			<View>
+
+			<View style={{ marginLeft: 20, marginTop: 35, marginBottom: 25 }}>
 				<Text>{item.descricao}</Text>
 			</View>
-			<View>
-				<Text>FOTOS</Text>
-			</View>
+
+			<TouchableOpacity onPress={() => false} 
+				style={{height: 25, 
+						marginHorizontal: 20, 
+						borderRadius: 3, 
+						borderWidth: 1 ,
+						borderColor: '#bfbfbf', 
+						justifyContent: 'center',
+				}}>
+				<Text style={{ fontWeight: 'bold', textAlign: 'center', borderColor: 'black' }}>Editar perfil</Text>
+			</TouchableOpacity>
+
 		</View>
 	);
 }
@@ -66,17 +92,21 @@ class Perfil extends Component {
 			<View style={{ backgroundColor: '#fff', flex: 1 }}>
 				<View style={{ backgroundColor: '#fff', }}>
 
-					<View style={{ height: 65, backgroundColor: '#fff', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderWidth: 1, borderColor: '#bfbfbf' }}>
-						<Text style={{ marginTop: 10 }}>Nome Usuario</Text>
+					<View style={{ height: 65, backgroundColor: '#fff', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', }}>
+						<Text style={{ marginTop: 15, fontWeight: 'bold' }}>Nome Usuario</Text>
 					</View>
 
 				</View>
 
-					<FlatList 
-						data={this.dataSource}
-						renderItem={ ({ item }) => <Informacoes item={item}/> }
-						keyExtractor={item => item.email}
-					/>
+				<FlatList 
+					data={this.dataSource}
+					renderItem={ ({ item }) => <Informacoes item={item}/> }
+					keyExtractor={item => item.email}
+				/>
+
+				<View style={{ flex: 10 }}>
+					<Text>FOTOS</Text>
+				</View>
 
 			</View>
 		)
