cef0fc6cf038bd1dc6e34ebab53b84dc6b77a1b9
GustavoBMendes <mendes.gustavob@gmail.com>
2020-07-07 12:06:39 -0300

    Iniciando seção explorar

--- src/Components/Explorar.js
+++ src/Components/Explorar.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 function Explorar({ navigation }) {
 	return (
-		<View>
-			<Text>Explorar</Text>
+		<View style={{ flex: 1, backgroundColor: '#fff', }}>
+			<View style={{ height: 65, backgroundColor: '#fff', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', borderWidth: 1, borderColor: '#bfbfbf' }}>
+
+				<TextInput placeholder='Pesquisar' 
+					style={{ 	alignItems: 'center', 
+								backgroundColor: '#fafafa', 
+								borderColor: '#f5f5f5', 
+								borderWidth: 1, 
+								borderRadius: 5, 
+								marginBottom: 10,
+								marginLeft: 15,
+								marginTop: 20,
+								height: 35,
+								width: 340 }}
+				/>
+
+			</View>
 		</View>
 	)
 }
