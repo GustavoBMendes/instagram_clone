2190478bb558eeb1e3ba625b9c84616045419345
GustavoBMendes <mendes.gustavob@gmail.com>
2020-07-08 19:28:56 -0300

    ajustes visuais

--- src/Components/Explorar.js
+++ src/Components/Explorar.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Image, Text, } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
+import Images from '../imgs/index';
 
 function Explorar({ navigation }) {
 	return (
 		<View style={{ flex: 1, backgroundColor: '#fff', }}>
-			<View style={{ height: 65, backgroundColor: '#fff', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', borderWidth: 1, borderColor: '#bfbfbf' }}>
+			<View style={{ height: 65, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'space-between', borderWidth: 1, borderColor: '#bfbfbf' }}>
 
-				<TextInput placeholder='Pesquisar' 
-					style={{ 	alignItems: 'center', 
-								backgroundColor: '#fafafa', 
-								borderColor: '#f5f5f5', 
-								borderWidth: 1, 
-								borderRadius: 5, 
-								marginBottom: 10,
-								marginLeft: 15,
-								marginTop: 20,
-								height: 35,
-								width: 340 }}
-				/>
+				<View style={{ height: 40, backgroundColor: '#fafafa', marginLeft: 15, marginRight: 15, marginTop: 20, borderColor: '#f5f5f5', borderWidth: 1, borderRadius: 10, flexDirection: 'row', }}>
+					<Image source={Images.search} style={{ height: 18, width: 18, marginTop: 10, marginLeft: 5, }}/>
+					<TextInput placeholder='Pesquisar' placeholderTextColor='#bfbfbf' style={styles.pesquisa} />
+				</View>
 
 			</View>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+
+	pesquisa: {
+		alignItems: 'center', 
+		justifyContent: 'center',
+		backgroundColor: '#fafafa', 
+		marginTop: 5,
+		marginLeft: 10,
+		height: 30,
+		width: 300,
+	}
+
+})
 
 export default Explorar;
